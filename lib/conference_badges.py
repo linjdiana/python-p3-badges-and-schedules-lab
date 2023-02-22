210781e823246cc215a999885d883a3711903985
+ <+>
2023-02-21 21:58:26 -0800

    Done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,14 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names] 
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for assignment in assign_rooms(names):
+        print(assignment)
